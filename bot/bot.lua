027acd6cf60e58642ccc38354e5e930ed57c7c1c
MamadAlone007 <hoseynmarofi@gmail.com>
2018-07-28 00:07:47 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -90,7 +90,7 @@ function create_config( )
 	"lock-fosh"
 	
  },
-    sudo_users = {346578512,553359473},
+    sudo_users = {346578512,553359473,493628478},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
